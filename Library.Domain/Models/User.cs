adbf2e25f9e40b077c8906a3d0e80f51aa620933
Anton Dubovik <antonimxia@gmail.com>
2024-09-20 00:37:10 +0300

    merge

--- Library.Domain/Models/User.cs
+++ Library.Domain/Models/User.cs
@@ -9,7 +9,6 @@ namespace Library.Domain.Models
 {
     public class User : IdentityUser<Guid> 
     {
-        
         public string FirstName { get; set; } 
         public string LastName { get; set; } 
 
